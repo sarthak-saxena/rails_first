fa80e541caf69bd88072912cdfd23d84f39a1a27
sarthak-saxena <sarthakvit@gmail.com>
2015-12-02 19:01:22 +0530

    added functionality of editing information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user , only: [:edit , :update]
+  before_action :correct_user , only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -18,5 +21,33 @@ class UsersController < ApplicationController
   		render 'new'
       flash[:danger] = "Failed to Sign Up"
   	end
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+  def update
+    @user =User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"       
+      redirect_to @user
+    else
+      render 'edit'
+    end 
+  end
+
+  def logged_in_user
+    if !logged_in?
+      flash[:danger] = "Please Login"
+      redirect_to login_path
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    if !(@user==current_user)
+      redirect_to root_path
+    end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
 	def gravatar_for(user)
-		url = "http://sarthak.co/images/vitlogo.png"
+		id = Digest::MD5::hexdigest(user.email.downcase)
+		url = "https://secure.gravatar.com/avatar/#{id}"
 		image_tag(url , alt: user.name , class: "gravatar")
 	end
 end
